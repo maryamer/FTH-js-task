051d9efc27fcef802cb8d18d5830438e629c0548
maryam <personalmaryamebrahimi@gmail.com>
2024-07-16 20:39:37 +0330

    json server deployement

--- src/js/indexOOP.js
+++ src/js/indexOOP.js
@@ -1,13 +1,3 @@
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json"); // <== Will be created later
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200; // <== You can change the port
-
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
 class TransactionsManager {
   constructor() {
     this.btn = document.querySelector(".btn");
